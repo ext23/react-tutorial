264c8a2910c757aad6cc6eef9d406a0b08ee9703
khe <a.khe@pdvfo.local>
2021-04-29 17:27:18 +0300

    add something

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Button, Menu, MenuItem } from '@material-ui/core';
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class AppMenu extends React.Component {
+    render(props) {
+        return <Menu id="appMenu" keepMounted>
+            <MenuItem>Статистика</MenuItem>
+            <MenuItem>Команды</MenuItem>
+            <MenuItem>Выход</MenuItem>
+        </Menu>
+    }
+}
+
+class Team extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,        
+        };        
+    }
+
+    sayHello() {
+        alert("Hello, " + this.state.name + "!");
+    }
+
+    render(props) {
+        return <Button color="primary" onClick={()=>this.sayHello()}>{this.props.name}</Button>;
+    }
+}
+
+function App() {
+    return (
+        <div>            
+            <AppMenu/>
+            <Team name="GENTLEMEN CARPFISHING CLUB"/>
+            <Team name="AQUA"/>
+            <Team name="OUR BROTHER CARP TEAM"/>        
+        </div>
+    );
+}
+
+reactDom.render(
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
